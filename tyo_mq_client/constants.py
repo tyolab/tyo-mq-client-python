de5f1ecd932bd8bfe8de88e1b64daf5f2d55a712
eric <eric@tyo.com.au>
2024-03-01 18:35:57 +1000

    WIP

--- tyo_mq_client/constants.py
+++ tyo_mq_client/constants.py
@@ -5,11 +5,19 @@
 class Constants:
 
     ANONYMOUS = 'ANONYMOUS'
+
     EVENT_DEFAULT = 'tyo-mq-mt-default'
+    EVENT_ALL = 'TM-ALL'
+
     SYSTEM = 'TYO-MQ-SYSTEM'
     ALL_PUBLISHERS = 'TYO-MQ-ALL'
 
     METHOD_BROADCAST = 'broadcast'
+
+    METHOD_UNICAST = 'unicast'
+
+    SCOPE_ALL = 'all'
+    SCOPE_DEFAULT = 'default'
 
     # SERVER DEFAULTS
     DEFAULT_PORT = 17352
--- tyo_mq_client/events.py
+++ tyo_mq_client/events.py
@@ -1,11 +1,61 @@
 #
 #
+from .constants import Constants
+    # public static string to_event_string(string event_name, string? prefix = null, string? suffix = null) {
+        
+    #     // eventStr = event
+    #     // TODO
+    #     // if (typeof event === "string") {
+    #     //     eventStr = event
+    #     // }
+    #     // else if (typeof event === "object" && event.event) {
+    #     //     eventStr = event.event
+    #     // }
+    #     // else 
+    #     //     throw new Error ("Unknown event object: should be a string or object with event string")
+    #     // return eventStr
+    #     return (null != prefix ? (prefix + '-') : "") + event_name + (null != suffix ? ('-' + suffix) : "");;
+    # }
+
+    # public static string to_consume_event(string event_name) {
+    #     return to_event_string(event_name, "CONSUME");
+    # }
+
+    # public static string to_consumer_event(string event_name, string prefix, bool is_all = false) {
+    #     if (is_all)
+    #         return to_event_string(event_name, prefix.ToLower());
+    #     return to_event_string(event_name, prefix).ToLower();
+    # }
+
+    # public static string to_ondisconnect_event(string id) {
+    #     return to_event_string(id, "DISCONNECT");
+    # }
+
+    # public static string to_onunsubscribe_event(string event_name, string id) {
+    #     // string eventStr = to_event_string(event_name);
+    #     // return "UNSUBSCRIBE-" + eventStr + "-" + id;
+    #     return to_event_string(event_name, "UNSUBSCRIBE", id);
+    # }
+
+    # public static string to_onsubscribe_event(string id) {
+    #     // return "SUBSCRIBE-TO" + ("-" + id != null ? id : "");
+    #     return to_event_string("TO", "SUBSCRIBE", id);
+    # }
+
+    # public static string to_consume_all_event(string producer) {
+    #     return to_event_string("CONSUME") + to_consumer_all_event(producer);
+    # }
+
+    # public static string to_consumer_all_event (string producer) {
+    #     // return 'producer + "-ALL";
+    #     return to_event_string(producer.ToLower(), null, Constants.EVENT_ALL);
+    # }
 
 class Events(object):
     #
     @staticmethod
-    def to_event_string(event):
-        eventStr = event
+    def to_event_string(event_name, prefix = None, suffix = None):
+        # eventStr = event
         # TODO
         # if (typeof event === 'string') {
         #     eventStr = event
@@ -15,22 +65,29 @@ class Events(object):
         # }
         # else 
         #     throw new Error ('Unknown event object: should be a string or object with event string')
-        return eventStr
+        return (None if (prefix is None) else (prefix + '-')) + event_name + (None if (suffix is None) else ('-' + suffix))
 
     @classmethod
     def to_consume_event(cls, event):
-        eventStr = cls.to_event_string(event)
-        return 'CONSUME-' + eventStr
+        return cls.to_event_string(event, 'CONSUME')
 
     @staticmethod
-    def to_ondisconnect_event(id):
-        return 'DISCONNECT-' + id
+    def to_ondisconnect_event(cls, id):
+        # return 'DISCONNECT-' + id
+        return cls.to_event_string(id, 'DISCONNECT')
 
     @classmethod
     def to_onunsubscribe_event(cls, event, id):
-        eventStr = cls.to_event_string(event)
-        return 'UNSUBSCRIBE-' + eventStr + '-' + id
+        # eventStr = cls.to_event_string(event)
+        # return 'UNSUBSCRIBE-' + eventStr + '-' + id
+        return cls.to_event_string(event, 'UNSUBSCRIBE', id)
 
     @staticmethod
-    def to_onsubscribe_event(id):
-        return 'SUBSCRIBE-TO' + ("-" + id if (id is not None) else "")
+    def to_onsubscribe_event(cls, id):
+        # return 'SUBSCRIBE-TO' + ("-" + id if (id is not None) else "")
+        return cls.to_event_string('TO', 'SUBSCRIBE', id)
+    
+    @staticmethod
+    def to_consumer_all_event (cls, producer):
+        # return 'producer + "-ALL"
+        return cls.to_event_string(producer.lower(), None, Constants.EVENT_ALL)
